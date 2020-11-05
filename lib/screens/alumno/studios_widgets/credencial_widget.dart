1b1052afddbd037e8b05d195a2af08eb8b267bdc
Jovanny Rch <jovannyrch@gmail.com>
2020-11-04 20:44:33 -0600

    Credencial UI [added]

--- lib/screens/alumno/studios_widgets/credencial_widget.dart
+++ lib/screens/alumno/studios_widgets/credencial_widget.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:control_escolar/const/const.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +12,11 @@ class CredencialWidget extends StatefulWidget {
 }
 
 class _CredencialWidgetState extends State<CredencialWidget> {
+  Size _size;
+
   @override
   Widget build(BuildContext context) {
+    _size = MediaQuery.of(context).size;
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -31,13 +37,127 @@ class _CredencialWidgetState extends State<CredencialWidget> {
   }
 
   Widget _cardContainer() {
-    return Center(
+    return Stack(
+      children: [
+        _credencialInfoContainer(),
+        _profileImg(),
+      ],
+    );
+  }
+
+  Widget _credencialInfoContainer() {
+    return Positioned(
+      top: 128.0,
       child: Container(
-        width: 263,
-        height: 357,
-        decoration: BoxDecoration(
-          color: kMainColor,
-          borderRadius: BorderRadius.circular(8.0),
+        width: _size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 263,
+              height: 357,
+              decoration: BoxDecoration(
+                color: kMainColor,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: _credencialInfo(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _credencialInfo() {
+    return Container(
+      child: Row(
+        children: [
+          _schoolName(),
+          SizedBox(width: 5.0),
+          _columnInfo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _columnInfo() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _titleField("Nombre del alumno"),
+        _valueField("Jovanny Ramírez Chimal"),
+        SizedBox(height: 8.0),
+        _titleField("Número de cuenta"),
+        _valueField("1521004"),
+        SizedBox(height: 18.0),
+        _titleField("Carrera"),
+        _valueField("Ingeniería en Computación"),
+      ],
+    );
+  }
+
+  Widget _valueField(String value, {double width = 175.0}) {
+    return Container(
+      width: width,
+      margin: EdgeInsets.only(top: 2.0),
+      child: Text(
+        value,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+          fontWeight: FontWeight.bold,
+        ),
+        maxLines: 2,
+      ),
+    );
+  }
+
+  Widget _titleField(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 10.0,
+        fontWeight: FontWeight.bold,
+        color: kSecondaryColor,
+      ),
+    );
+  }
+
+  Widget _schoolName() {
+    return Container(
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Container(
+          height: 75.0,
+          child: Text(
+            "EPO 60",
+            style: TextStyle(
+              fontSize: 64.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white.withOpacity(0.16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImg() {
+    return Positioned(
+      top: 64.0,
+      child: Container(
+        width: _size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 64,
+              backgroundColor: Colors.white,
+              child: Image.asset("assets/images/avatar.png"),
+            ),
+          ],
         ),
       ),
     );
