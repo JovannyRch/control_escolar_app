b80769eaaf8af4aba1a6b6594898399be6d456b0
Jovanny Rch <jovannyrch@gmail.com>
2020-11-04 19:50:45 -0600

    Materias UI [added]

--- lib/screens/alumno/studios_widgets/credencial_widget.dart
+++ lib/screens/alumno/studios_widgets/credencial_widget.dart
@@ -1,3 +1,4 @@
+import 'package:control_escolar/const/const.dart';
 import 'package:flutter/material.dart';
 
 class CredencialWidget extends StatefulWidget {
@@ -10,6 +11,35 @@ class CredencialWidget extends StatefulWidget {
 class _CredencialWidgetState extends State<CredencialWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Credencial"));
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [
+            0.12,
+            1.0,
+          ],
+          colors: [
+            Color(0xFFF8F8F8),
+            Color(0xFF52DE97),
+          ],
+        ),
+      ),
+      child: _cardContainer(),
+    );
+  }
+
+  Widget _cardContainer() {
+    return Center(
+      child: Container(
+        width: 263,
+        height: 357,
+        decoration: BoxDecoration(
+          color: kMainColor,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    );
   }
 }
